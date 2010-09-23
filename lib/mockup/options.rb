83a44cfa2d8eb72a390f9cda2fbbd8e9d423fd22
Robert R Evans <robert@codewranglers.org>
2010-09-23 10:05:04 -0700

    Updated read me and option parser

--- lib/mockup/options.rb
+++ lib/mockup/options.rb
@@ -5,6 +5,9 @@ module Mockup
       options = OpenStruct.new
       
       opts = ::OptionParser.new do |opts|
+        
+        opts.banner "Usage: You can just do 'mockup' and it will create a directory in your current working directory named 'mockup'. Or you can pass in options: mockup mockup_name -l /path/to/mockup"
+        
         opts.on('-l', '--location LOCATION', 'The directory location you want your mockup to be created in. This defaults to the current working directory.') do |location|
           options.location = location
         end
